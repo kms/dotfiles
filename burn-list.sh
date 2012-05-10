03ae84cf926c50c4132f0ef6c4cc5a3ce24e4997
Karl-Martin Skontorp <kms@skontorp.net>
2012-05-10 23:34:38 +0200

    Increase buffer for CD/DVD burner.

--- burn-list.sh
+++ burn-list.sh
@@ -4,4 +4,4 @@ GENISO="genisoimage -D -r --joliet-long -graft-points -path-list $2"
 
 blocks=`$GENISO --print-size --quiet`
 
-$GENISO | wodim -vv fs=96m tsize=${blocks}s speed=$1 -ignsize -eject -
+$GENISO | wodim -vv fs=256m tsize=${blocks}s speed=$1 -ignsize -eject -
